31bd3033c552afa1eff0d1dbcc36085fd6990363
Linx <olga-reshetova@mail.ru>
2024-04-17 17:18:13 +0700

    рефакторинг main-nav

---
+++ source/js/blur.js
@@ -0,0 +1,19 @@
+const BLUR_ACTIVE_CLASS = 'blur--active';
+
+const blur = document.querySelector('.blur');
+
+export const toogleBlur = () => {
+  blur.classList.toggle(BLUR_ACTIVE_CLASS);
+}
+
+export const activateBlur = () => {
+  if (!blur.classList.contains(BLUR_ACTIVE_CLASS)) {
+    blur.classList.add(BLUR_ACTIVE_CLASS)
+  }
+}
+
+export const deactivateBlur = () => {
+  if (!blur.classList.contains(BLUR_ACTIVE_CLASS)) {
+    blur.classList.remove(BLUR_ACTIVE_CLASS)
+  }
+}
--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -1,3 +1,5 @@
+import { toogleBlur, activateBlur, deactivateBlur } from "./blur.js";
+
 const MAIN_NAV_CLASS = 'main-nav';
 const MAIN_NAV_OPENED_CLASS = 'main-nav--opened';
 
@@ -5,21 +7,18 @@ const SITE_TOGGLE_CLASS = 'site-toggle';
 const SITE_TOGGLE_CLOSED_CLASS = 'site-toggle--closed';
 const SITE_TOGGLE_OPENED_CLASS = 'site-toggle--opened';
 
-const SITE_LIST_CLASS = 'site-list';
-const SITE_LIST_DESKTOP_CLASS = 'site-list--desktop';
-
 const mainNav = document.querySelector(`.${MAIN_NAV_CLASS}`)
 const siteToogle = mainNav.querySelector(`.${SITE_TOGGLE_CLASS}`);
-const siteList = mainNav.querySelector(`.${SITE_LIST_CLASS}`);
-
 
 siteToogle.addEventListener('click', () => {
   if (siteToogle.classList.contains(SITE_TOGGLE_CLOSED_CLASS)) {
     siteToogle.classList.remove(SITE_TOGGLE_CLOSED_CLASS);
     siteToogle.classList.add(SITE_TOGGLE_OPENED_CLASS)
+    activateBlur();
   } else {
     siteToogle.classList.remove(SITE_TOGGLE_OPENED_CLASS);
     siteToogle.classList.add(SITE_TOGGLE_CLOSED_CLASS);
+    deactivateBlur();
   }
 
   mainNav.classList.toggle(MAIN_NAV_OPENED_CLASS);
